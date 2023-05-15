178e6858c1e6ef9f5a0d44172dab9b34d31cd255
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-05-15 12:21:27 +0900

    Replace path seperator for platform

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/mattn/go-colorable"
 
@@ -14,6 +16,22 @@ import (
 	"github.com/nyaosorg/go-readline-ny/simplehistory"
 	"github.com/zetamatta/go-shellcommand"
 )
+
+const spaces = "\t\n\v\f\r "
+
+func cutField(source string) (first, rest string) {
+	source = strings.TrimLeft(source, spaces)
+	var quote bool
+	for i, r := range source {
+		if r == '"' {
+			quote = !quote
+		}
+		if !quote && strings.IndexRune(spaces, r) >= 0 {
+			return source[:i], source[i:]
+		}
+	}
+	return source, ""
+}
 
 func mains() error {
 	history := simplehistory.New()
@@ -35,6 +53,9 @@ func mains() error {
 			}
 			return err
 		}
+		cmd, arg := cutField(text)
+		cmd = filepath.FromSlash(cmd)
+		text = cmd + arg
 
 		process, err := shellcommand.System(text)
 		if err != nil {
